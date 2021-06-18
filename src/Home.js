5217978dfc9a6310ef4e73c66dc54602d05b7139
getromandev <getroman.dev@gmail.com>
2021-06-18 15:47:52 -0400

    fetch data and remove static event handlers

--- src/Home.js
+++ src/Home.js
@@ -1,25 +1,27 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
 
-    const [blogs, setBlogs] = useState([
-        { title: 'My new website', body: 'the web indeed...', author: 'Heriberto', id: 1},
-        { title: 'My new website', body: 'the web indeed...', author: 'Heriberto', id: 2},
-        { title: 'My new website', body: 'the web indeed...', author: 'Eli', id: 3},
-    ])
+    const [blogs, setBlogs] = useState(null)
 
-    const handleDelete = (id) => {
-        // filter out the blog based on the ID 
-        const newBlogs = blogs.filter(blog => blog.id !== id)
-        // update the state
-        setBlogs(newBlogs)
-    }
+    useEffect(() => {
+        console.log('API Fetched from useffect Hook')
+        fetch('http://localhost:8000/blogs')
+            .then(res => {
+                console.log('res', res)
+                return res.json();
+            })
+            .then(data => {
+                console.log('json data', data)
+                setBlogs(data)
+            })
+    }, [])
 
     return (
         <div className='home'>
-            <BlogList blogs={ blogs } title='Built By Getroman' handleDelete={ handleDelete } />
-            <BlogList blogs={ blogs.filter((blog) => blog.author === 'Eli') } title='Built By Getroman' handleDelete={ handleDelete } />
+            { blogs && <BlogList blogs={ blogs } title='Built By Getroman' /> }
+            { blogs && <BlogList blogs={ blogs.filter((blog) => blog.author === 'Eli') } title='Built By Getroman' /> }
         </div>
     );
 }
