227f186e983b7d46abca3c9290ed9568c2bfde81
getromandev <getroman.dev@gmail.com>
2021-06-17 23:49:11 -0400

    add button and state to home component

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,26 @@
+import { useState } from "react";
+
 const Home = () => {
+
+    const [name, setName] = useState('Eli');
+    const [age, setAge] = useState(8);
+
+    const handleClick = () => {
+        console.log('Built By Getroman')
+        setName('Heriberto');
+        setAge(39);
+    }
+
+    const handleClickAgain = (name, e) => {
+        console.log('Hello ' + name, e.target);
+    }
+
     return (
         <div className='home'>
             <h2>Homepage</h2>
+            <p>{ name } is { age } years old</p>
+            <button onClick={ handleClick } >Click me</button>
+            <button onClick={(e) => handleClickAgain('Getroman', e)}>Click me again</button>
         </div>
     );
 }
