19cdeca204f3e9128e93ac8a92e40aa721d03e4d
getromandev <getroman.dev@gmail.com>
2021-06-18 00:12:47 -0400

    output list and add css

--- src/Home.js
+++ src/Home.js
@@ -2,25 +2,20 @@ import { useState } from "react";
 
 const Home = () => {
 
-    const [name, setName] = useState('Eli');
-    const [age, setAge] = useState(8);
-
-    const handleClick = () => {
-        console.log('Built By Getroman')
-        setName('Heriberto');
-        setAge(39);
-    }
-
-    const handleClickAgain = (name, e) => {
-        console.log('Hello ' + name, e.target);
-    }
+    const [blogs, setBlogs] = useState([
+        { title: 'My new website', body: 'the web indeed...', author: 'Heriberto', id: 1},
+        { title: 'My new website', body: 'the web indeed...', author: 'Heriberto', id: 2},
+        { title: 'My new website', body: 'the web indeed...', author: 'Heriberto', id: 3},
+    ])
 
     return (
         <div className='home'>
-            <h2>Homepage</h2>
-            <p>{ name } is { age } years old</p>
-            <button onClick={ handleClick } >Click me</button>
-            <button onClick={(e) => handleClickAgain('Getroman', e)}>Click me again</button>
+            {blogs.map((blog) => (
+                <div className='blog-preview' key={blog.id}>
+                    <h2>{ blog.title }</h2>
+                    <p>Written by { blog.author }</p>
+                </div>
+            ))}
         </div>
     );
 }
