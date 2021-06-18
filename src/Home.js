5e227801eca12b1afc83b27d6bf3859d2e10914c
getromandev <getroman.dev@gmail.com>
2021-06-18 13:08:44 -0400

    add delete functionality by passing the handleDelete function as a prop

--- src/Home.js
+++ src/Home.js
@@ -9,10 +9,17 @@ const Home = () => {
         { title: 'My new website', body: 'the web indeed...', author: 'Eli', id: 3},
     ])
 
+    const handleDelete = (id) => {
+        // filter out the blog based on the ID 
+        const newBlogs = blogs.filter(blog => blog.id !== id)
+        // update the state
+        setBlogs(newBlogs)
+    }
+
     return (
         <div className='home'>
-            <BlogList blogs={ blogs } title='Built By Getroman' />
-            <BlogList blogs={ blogs.filter((blog) => blog.author === 'Eli') } title='Built By Getroman' />
+            <BlogList blogs={ blogs } title='Built By Getroman' handleDelete={ handleDelete } />
+            <BlogList blogs={ blogs.filter((blog) => blog.author === 'Eli') } title='Built By Getroman' handleDelete={ handleDelete } />
         </div>
     );
 }
