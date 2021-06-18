8f19ce5c37c22ebe9f009e5067c283c95983cdbb
getromandev <getroman.dev@gmail.com>
2021-06-18 12:39:35 -0400

    add second bloglist component to home and filter out blog post

--- src/Home.js
+++ src/Home.js
@@ -6,12 +6,13 @@ const Home = () => {
     const [blogs, setBlogs] = useState([
         { title: 'My new website', body: 'the web indeed...', author: 'Heriberto', id: 1},
         { title: 'My new website', body: 'the web indeed...', author: 'Heriberto', id: 2},
-        { title: 'My new website', body: 'the web indeed...', author: 'Heriberto', id: 3},
+        { title: 'My new website', body: 'the web indeed...', author: 'Eli', id: 3},
     ])
 
     return (
         <div className='home'>
-            <BlogList blogs={blogs} title='Built By Getroman' />
+            <BlogList blogs={ blogs } title='Built By Getroman' />
+            <BlogList blogs={ blogs.filter((blog) => blog.author === 'Eli') } title='Built By Getroman' />
         </div>
     );
 }
