032bd71418fd03372a26a4ea6113dcdca25a1385
getromandev <getroman.dev@gmail.com>
2021-06-21 15:56:48 -0400

    add fetch error logic

--- src/Home.js
+++ src/Home.js
@@ -5,23 +5,35 @@ const Home = () => {
 
     const [blogs, setBlogs] = useState(null)
     const [isLoading, setIsLoading] = useState(true)
+    const [error, setError] = useState(null);
 
     useEffect(() => {
         console.log('API Fetched from useffect Hook')
         fetch('http://localhost:8000/blogs')
             .then(res => {
                 console.log('res', res)
+                // if the response is not ok throw error 
+                if (!res.ok) {
+                    throw Error('could not fetch the data for that resource')
+                }
                 return res.json();
             })
             .then(data => {
                 console.log('json data', data)
-                setBlogs(data)
-                setIsLoading(false)
+                setBlogs(data);
+                setIsLoading(false);
+                setError(null);
+            })
+            .catch(err => {
+                console.log(err.message);
+                setError(err.message);
+                setIsLoading(false);
             })
     }, [])
 
     return (
         <div className='home'>
+            {error && <div>{ error }</div>}
             { isLoading && <div>Loading...</div>}
             { blogs && <BlogList blogs={ blogs } title='Built By Getroman' /> }
             { blogs && <BlogList blogs={ blogs.filter((blog) => blog.author === 'Eli') } title='Built By Getroman' /> }
