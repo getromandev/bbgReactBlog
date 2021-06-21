c8a5e9c5584a18435076b6598257744fcd6f6374
getromandev <getroman.dev@gmail.com>
2021-06-21 17:00:04 -0400

    import and use cutom fetch hook

--- src/Home.js
+++ src/Home.js
@@ -1,35 +1,9 @@
 import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
+import useFetch from "./useFetch";
 
 const Home = () => {
-
-    const [blogs, setBlogs] = useState(null)
-    const [isLoading, setIsLoading] = useState(true)
-    const [error, setError] = useState(null);
-
-    useEffect(() => {
-        console.log('API Fetched from useffect Hook')
-        fetch('http://localhost:8000/blogs')
-            .then(res => {
-                console.log('res', res)
-                // if the response is not ok throw error 
-                if (!res.ok) {
-                    throw Error('could not fetch the data for that resource')
-                }
-                return res.json();
-            })
-            .then(data => {
-                console.log('json data', data)
-                setBlogs(data);
-                setIsLoading(false);
-                setError(null);
-            })
-            .catch(err => {
-                console.log(err.message);
-                setError(err.message);
-                setIsLoading(false);
-            })
-    }, [])
+    const { data: blogs, isLoading, error } = useFetch('http://localhost:8000/blogs');
 
     return (
         <div className='home'>
