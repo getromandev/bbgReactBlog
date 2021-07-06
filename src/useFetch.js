db7ff9d2740b08522c682bad4584fbed70f7c98a
getromandev <getroman.dev@gmail.com>
2021-07-06 19:59:12 -0400

    add useEffect Cleanup functionality to prevent fetch errors

--- src/useFetch.js
+++ src/useFetch.js
@@ -6,27 +6,38 @@ const useFetch = (url) => {
     const [error, setError] = useState(null);
 
     useEffect(() => {
+        const abortCont = new AbortController();
+
         console.log('API Fetched from useffect Hook')
-        fetch(url)
-            .then(res => {
-                console.log('res', res)
-                // if the response is not ok throw error 
-                if (!res.ok) {
-                    throw Error('could not fetch the data for that resource')
-                }
-                return res.json();
-            })
-            .then(data => {
-                console.log('json data', data)
-                setData(data);
-                setIsLoading(false);
-                setError(null);
-            })
-            .catch(err => {
-                console.log(err.message);
-                setError(err.message);
-                setIsLoading(false);
-            })
+        
+        setTimeout(() => {
+            fetch(url, { signal: abortCont.signal })
+                .then(res => {
+                    console.log('res', res)
+                    // if the response is not ok throw error 
+                    if (!res.ok) {
+                        throw Error('could not fetch the data for that resource')
+                    }
+                    return res.json();
+                })
+                .then(data => {
+                    console.log('json data', data)
+                    setData(data);
+                    setIsLoading(false);
+                    setError(null);
+                })
+                .catch(err => {
+                    if (err.name === 'AbortError') {
+                        console.log('Fetch Aborted')
+                    } else {
+                        console.log(err.message);
+                        setError(err.message);
+                        setIsLoading(false);
+                    }
+                })
+        }, 1000)
+
+            return () => abortCont.abort();
     }, [url])
 
     return { data, isLoading, error}
