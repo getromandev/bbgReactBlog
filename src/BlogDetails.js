450dea46d448b42f114db8a95afc7cafb6cd689a
getromandev <getroman.dev@gmail.com>
2021-07-07 15:13:07 -0400

    implement and reuse fetch hook to provide blog details

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,12 +1,23 @@
+
 import { useParams } from "react-router";
+import useFetch from './useFetch';
 
 const BlogDetails = () => {
     const { id } = useParams();
+    const { data: blog, error, isPending} = useFetch(`http://localhost:8000/blogs/${id}`);
 
     return ( 
         <div className='blog-details'>
-            <h2>Blog</h2>
-            <p>The id of the blog post - { id }</p>
+            { isPending && <div>Loading ...</div> } 
+            { error && <div> { error } </div> }
+            { blog && (
+                <article>
+                    <h2>{ blog.title }</h2>
+                    <p>Written by { blog.author }</p>
+                    <div>{ blog.body }</div>
+                </article> 
+            )
+            }
         </div>
      );
 }
