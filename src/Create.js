75b5fa95e7b23504fb42959d78055339ad9a17ee
getromandev <getroman.dev@gmail.com>
2021-07-08 18:10:28 -0400

    create form

--- src/Create.js
+++ src/Create.js
@@ -3,6 +3,18 @@ const Create = () => {
         <div className='create'>
             <h2>Add a New Blog</h2>
             <p>Deatails go here</p>
+            <form>
+                <label>Blog Title:</label>
+                <input type='text' required />
+                <label>Blog Body:</label>
+                <textarea required></textarea>
+                <label>Blog Author</label>
+                <select>
+                    <option value='mario'>mario</option>
+                    <option value='yoshi'>yoshi</option>
+                </select>
+                <button>Add Blog</button>
+            </form>
         </div>
      );
 }
