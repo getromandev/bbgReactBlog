bb1fa3ffe04d32180754cb2c43cfc94f41d2da2b
getromandev <getroman.dev@gmail.com>
2021-07-08 22:20:50 -0400

    add state to the blog creation form

--- src/Create.js
+++ src/Create.js
@@ -1,19 +1,41 @@
+import { useState } from "react";
+
 const Create = () => {
+    const [title, setTitle] = useState('');
+    const [bodyText, setBodyText] = useState('');
+    const [selectValue, setSelectValue] = useState('mario');
+
     return ( 
         <div className='create'>
             <h2>Add a New Blog</h2>
             <p>Deatails go here</p>
             <form>
                 <label>Blog Title:</label>
-                <input type='text' required />
+                <input 
+                    type='text'
+                    value={title}
+                    onChange={(e) => setTitle(e.target.value)}
+                    required 
+                />
                 <label>Blog Body:</label>
-                <textarea required></textarea>
+                <textarea 
+                    value={bodyText}
+                    onChange={(e) => setBodyText(e.target.value)}
+                    required                
+                >
+                </textarea>
                 <label>Blog Author</label>
-                <select>
+                <select 
+                    value={selectValue} 
+                    onChange={(e) => setSelectValue(e.target.value)}
+                >
                     <option value='mario'>mario</option>
                     <option value='yoshi'>yoshi</option>
                 </select>
                 <button>Add Blog</button>
+                <p>{title}</p>
+                <p>{bodyText}</p>
+                <p>{selectValue}</p>
             </form>
         </div>
      );
